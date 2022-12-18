a71e50416847ce2b0d6f6aa73eafc0d322fdfa71
Sandip Ranjan Behera <B320043@iiit-bh.ac.in>
2022-12-18 12:37:22 +0530

    Update SearchBar.jsx

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -33,11 +33,11 @@ const SearchBar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch })
           width={20}
           height={20}
           alt="search"
-          className={theme === 'light' ? 'filter invert' : undefined}
+          className={theme === 'light' ? 'filter invert' : ''}
         />
         <input
           type="text"
-          placeholder="Search item here"
+          placeholder="Search NFT here"
           className="dark:bg-nft-black-2 bg-white mx-4 w-full font-poppins dark:text-white text-nft-black-1 font-normal text-xs outline-none"
           onChange={(e) => setDebouncedSearch(e.target.value)}
           value={debouncedSearch}
@@ -45,7 +45,7 @@ const SearchBar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch })
       </div>
 
       <div
-        onClick={() => setToggle(!toggle)}
+        onClick={() => setToggle((prevToggle) => !prevToggle)}
         className="relative flexBetween ml-4 sm:ml-0 sm:mt-2 min-w-190 cursor-pointer dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md"
       >
         <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-xs">{activeSelect}</p>
@@ -55,7 +55,7 @@ const SearchBar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch })
           width={15}
           height={15}
           alt="arrow"
-          className={theme === 'light' ? 'filter invert' : undefined}
+          className={theme === 'light' ? 'filter invert' : ''}
         />
 
         {toggle && (
