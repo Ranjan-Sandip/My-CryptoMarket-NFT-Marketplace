9ab64893092cc92304a05c5f388ee8cdd9359dff
Sandip Ranjan Behera <B320043@iiit-bh.ac.in>
2022-12-18 12:33:43 +0530

    Update Input.jsx

--- components/Input.jsx
+++ components/Input.jsx
@@ -21,7 +21,12 @@ const Input = ({ inputType, title, placeholder, handleClick }) => {
         </div>
       )
         : inputType === 'textarea' ? (
-          <textarea rows={10} className="dark:bg-nft-black-1 bg-white border dark:border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-4 py-3" placeholder={placeholder} onChange={handleClick} />
+          <textarea
+            rows={10}
+            className="dark:bg-nft-black-1 bg-white border dark:border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-4 py-3"
+            placeholder={placeholder}
+            onChange={handleClick}
+          />
         )
           : (
             <input
