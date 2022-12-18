88ec6015e778f69987efbba43f776d946c1d7473
Sandip Ranjan Behera <B320043@iiit-bh.ac.in>
2022-12-18 12:31:09 +0530

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Banner = ({ name, childStyles, parentStyle }) => (
   <div className={`relative w-full flex items-center z-0 overflow-hidden nft-gradient ${parentStyle}`}>
     <p className={`font-bold text-5xl text-white font-poppins leading-70 ${childStyles}`}>{name}</p>
